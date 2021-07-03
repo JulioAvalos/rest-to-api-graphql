395e98acfb26674cf2cbe091fa99d3ae42f82905
JulioAvalos <julioavalos2@msn.com>
2021-07-03 11:03:08 -0600

    adding main datasource

--- src/server.ts
+++ src/server.ts
@@ -6,33 +6,40 @@ import schema from './schema';
 import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 import expressPlayGround from 'graphql-playground-middleware-express';
+import { dataSources } from './data';
 
 async function init() {
-    // Inicializamos la aplicación express
-    const app = express();
+  // Inicializamos la aplicación express
+  const app = express();
 
-    // Añadimos configuración de Cors y compression
-    app.use(cors());
+  // Añadimos configuración de Cors y compression
+  app.use(cors());
 
-    app.use(compression());
+  app.use(compression());
 
-    // Inicializamos el servidor de Apollo
-    const server = new ApolloServer({
-        schema,
-        introspection: true // Necesario
-    });
+  // Inicializamos el servidor de Apollo
+  const server = new ApolloServer({
+    schema,
+    introspection: true, // Necesario
+    dataSources: () => ({ seasons: new dataSources.SeasonsData() }),
+  });
 
-    server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
-    app.use('/', expressPlayGround({
-        endpoint: '/graphql'
-    }));
+  app.use(
+    '/',
+    expressPlayGround({
+      endpoint: '/graphql',
+    })
+  );
 
-    const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
 
-    const httpServer = createServer(app);
+  const httpServer = createServer(app);
 
-    httpServer.listen({ port: PORT }, (): void => console.log(`http://localhost:${PORT}/graphql`));
+  httpServer.listen({ port: PORT }, (): void =>
+    console.log(`http://localhost:${PORT}/graphql`)
+  );
 }
 
 init();
