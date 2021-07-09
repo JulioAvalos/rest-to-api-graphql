aefd75a8a2d58d3e4133ea16e49c4cbd21930fa9
JulioAvalos <julioavalos2@msn.com>
2021-07-08 21:55:55 -0600

    adding circuit datasource

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,9 +1,11 @@
 import { SeasonsData } from "./data-seasons";
 import { RacesData } from "./data-races";
 import { DriversData } from './data-drivers';
+import { CircuitData } from './data-circuit';
 
 export const dataSources = {
     SeasonsData,
     RacesData,
-    DriversData
+    DriversData,
+    CircuitData
 };
